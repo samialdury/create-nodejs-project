f32d3b817a745efd4cd9f45708c416a2970eebb1
Sami Al-Dury <57627858+samialdury@users.noreply.github.com>
2023-01-22 23:56:47 +0100

    chore: logger instance debug logs

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -11,9 +11,13 @@ export const init = ({ name }: { name: string }): void => {
 	})
 
 	logger.useLevelLabels = true
+
+	logger.debug('Logger initialized')
 }
 
 export const destroy = (): void => {
+	logger.debug('Logger instance destroyed')
+
 	// @ts-expect-error For test teardown
 	logger = undefined
 }
